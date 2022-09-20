2d7fae9c15214d909204172ce07c801c4a9b2ddd
diegodisant <developerdiego0@gmail.com>
2022-09-20 06:28:18 -0500

    feat: implement bootloader :rocket:

---
+++ src/bootloader/bootloader.s
@@ -0,0 +1,26 @@
+.section multiboot
+.align 4
+.long ALIGN
+.long FLAGS
+.long CHECKSUM
+
+.section bss
+.align 16
+
+stack_bottom:
+.skip 16384
+
+stack_top:
+.section .text
+.global _start
+.type _start, @function
+_start:
+mov $stack_top, %esp
+
+call kernelMain
+cli
+
+1:hlt
+  jmp 1b
+
+.size _start, . - _start
